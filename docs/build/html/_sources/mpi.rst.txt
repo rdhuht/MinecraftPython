ac86902beda5d8b4d604c78a961f2b9e076b711e
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2021-03-02 13:48:03 +0800

    docs

---
+++ docs/build/html/_sources/mpi.rst.txt
@@ -0,0 +1,93 @@
+mpi package
+===========
+
+Submodules
+----------
+
+mpi.block module
+----------------
+
+.. automodule:: mpi.block
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.connection module
+---------------------
+
+.. automodule:: mpi.connection
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.entity module
+-----------------
+
+.. automodule:: mpi.entity
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.event module
+----------------
+
+.. automodule:: mpi.event
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.fonts module
+----------------
+
+.. automodule:: mpi.fonts
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.minecraft module
+--------------------
+
+.. automodule:: mpi.minecraft
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.minecraftstuff module
+-------------------------
+
+.. automodule:: mpi.minecraftstuff
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.text module
+---------------
+
+.. automodule:: mpi.text
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.util module
+---------------
+
+.. automodule:: mpi.util
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mpi.vec3 module
+---------------
+
+.. automodule:: mpi.vec3
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: mpi
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath('../mpi'))
 
 # -- Project information -----------------------------------------------------
 
@@ -24,19 +24,13 @@ author = 'rdhuht'
 # The full version, including alpha/beta/rc tags
 release = '1.0.1'
 
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,13 +41,13 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
